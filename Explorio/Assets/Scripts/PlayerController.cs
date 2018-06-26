816be32771575148577f01f15d8bf5027c8295e7
Pumas2222 <34688504+Pumas2222@users.noreply.github.com>
2018-06-26 18:16:47 +0100

    Update

---
+++ Explorio/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float moveSpeed;
+    
+    private void Start()
+    {
+        moveSpeed = 1f;
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
