9f882fb89f8305af870c2adc0e31c84f421fbb20
Pumas2222 <34688504+Pumas2222@users.noreply.github.com>
2018-06-27 21:50:36 +0100

    Made camera better

---
+++ Explorio/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraHeight;
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y += cameraHeight;
+        pos.z -= cameraHeight * 2f;
+        transform.position = pos;
+    }
+}
