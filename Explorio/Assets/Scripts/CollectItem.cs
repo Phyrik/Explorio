40674d88a77db11592f370428b6b5e2540be0007
Pumas2222 <34688504+Pumas2222@users.noreply.github.com>
2018-06-28 20:15:08 +0100

    Added collect item feature

---
+++ Explorio/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectItem : MonoBehaviour {
+
+    public int wood = 0;
+    public Text woodCountText;
+
+    private void Update()
+    {
+        SetWoodCountText();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Mixed_Palm_tree_01")
+        {
+            Destroy(col.gameObject);
+            wood += 1;
+        }
+    }
+
+    void SetWoodCountText()
+    {
+        woodCountText.text = "Wood: " + wood;
+    }
+}
