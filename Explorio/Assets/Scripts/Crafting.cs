505e26b64112adbd11287ff7c33e382f0fe9933a
Pumas2222 <34688504+Pumas2222@users.noreply.github.com>
2018-07-01 19:14:21 +0100

    Added rock and crafting

---
+++ Explorio/Assets/Scripts/Crafting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour {
+
+    public GameObject craftingPanel;
+
+    public void craftingButtonPressed ()
+    {
+        craftingPanel.SetActive(true);
+    }
+}
