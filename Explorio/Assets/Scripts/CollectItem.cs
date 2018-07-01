8eec602111ce03b2812f0a49c2f99e28c9174af0
Pumas2222 <34688504+Pumas2222@users.noreply.github.com>
2018-07-01 19:22:05 +0100

    Added ability to collect stone

--- Explorio/Assets/Scripts/CollectItem.cs
+++ Explorio/Assets/Scripts/CollectItem.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class CollectItem : MonoBehaviour {
 
     public int wood = 0;
+    public int stone = 0;
     public Text woodCountText;
+    public Text stoneCountText;
 
     private void Update()
     {
-        SetWoodCountText();
+        SetMaterialsCountText();
     }
 
     void OnCollisionEnter(Collision col)
@@ -20,10 +22,17 @@ public class CollectItem : MonoBehaviour {
             Destroy(col.gameObject);
             wood += 1;
         }
+
+        if(col.gameObject.name == "Mixed_Rock_01")
+        {
+            Destroy(col.gameObject);
+            stone += 1;
+        }
     }
 
-    void SetWoodCountText()
+    void SetMaterialsCountText()
     {
         woodCountText.text = "Wood: " + wood;
+        stoneCountText.text = "Stone: " + stone;
     }
 }
