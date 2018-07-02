b00e6d77e8a139cb84804102eb4f6c7a077b0620
Pumas2222 <34688504+Pumas2222@users.noreply.github.com>
2018-07-02 14:34:54 +0100

    Made crafting better and moved "Stone: " and "Wood: " to crafting panel

--- Explorio/Assets/Scripts/Crafting.cs
+++ Explorio/Assets/Scripts/Crafting.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Crafting : MonoBehaviour {
 
     public GameObject craftingPanel;
+    public Text craftingButtonText;
 
-    public void craftingButtonPressed ()
+    private void Start()
     {
-        craftingPanel.SetActive(true);
+        craftingPanel.SetActive(false);
+    }
+
+    void craftingButtonPressed ()
+    {
+        craftingPanel.SetActive(!craftingPanel.activeSelf);
+        GetComponent<Button>().enabled = false;
+        GetComponent<Button>().enabled = true;
+
+        Debug.Log(craftingButtonText.text);
+
+        if(craftingButtonText.text == "Craft")
+        {
+            Debug.Log("1");
+            craftingButtonText.text = "Close";
+        }
+
+        else if (craftingButtonText.text == "Close")
+        {
+            Debug.Log("2");
+            craftingButtonText.text = "Craft";
+        }
     }
 }
