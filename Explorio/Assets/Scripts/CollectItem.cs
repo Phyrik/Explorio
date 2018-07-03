d47939a97cd9552738a1c33a34c389a83955a716
Phyrik <34688504+Phyrik@users.noreply.github.com>
2018-07-03 17:10:18 +0100

    Added ability to buy hammer

--- Explorio/Assets/Scripts/CollectItem.cs
+++ Explorio/Assets/Scripts/CollectItem.cs
@@ -5,13 +5,34 @@ using UnityEngine.UI;
 
 public class CollectItem : MonoBehaviour {
 
-    public int wood = 0;
-    public int stone = 0;
+    public GameObject craftingPanel;
+    public GameObject hammer;
+    public GameObject craftingButton;
+    public Text craftingButtonText;
     public Text woodCountText;
     public Text stoneCountText;
+    public bool hammerBought;
+    public int wood = 0;
+    public int stone = 0;
+
+    private void Start()
+    {
+        craftingPanel.SetActive(false);
+        hammerBought = false;
+    }
 
     private void Update()
     {
+        if (hammerBought == true)
+        {
+            hammer.SetActive(true);
+        }
+
+        if (hammerBought == false)
+        {
+            hammer.SetActive(false);
+        }
+
         SetMaterialsCountText();
     }
 
@@ -27,6 +48,33 @@ public class CollectItem : MonoBehaviour {
         {
             Destroy(col.gameObject);
             stone += 1;
+        }
+    }
+
+    public void craftingButtonPressed()
+    {
+        craftingPanel.SetActive(!craftingPanel.activeSelf);
+        craftingButton.GetComponent<Button>().enabled = false;
+        craftingButton.GetComponent<Button>().enabled = true;
+
+        if (craftingButtonText.text == "Craft")
+        {
+            craftingButtonText.text = "Close";
+        }
+
+        else if (craftingButtonText.text == "Close")
+        {
+            craftingButtonText.text = "Craft";
+        }
+    }
+
+    public void buyHammer()
+    {
+        if (hammerBought == false && wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            hammerBought = true;
         }
     }
 
