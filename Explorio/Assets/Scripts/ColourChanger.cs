4b9a9165775eeb54ddb8a502d99bc27fca647be1
Phyrik <34688504+Phyrik@users.noreply.github.com>
2018-07-04 19:29:54 +0100

    Added change color button (partly functioning)

---
+++ Explorio/Assets/Scripts/ColourChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChanger : MonoBehaviour {
+
+    public Material material;
+    public Color playerColour;
+
+	public void changeColour ()
+    {
+        material.color = playerColour;
+    }
+}
